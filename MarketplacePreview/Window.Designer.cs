c7c66d4ac336b1c21450065f9c92634b7281b11c
Wither <wither.tweets@gmail.com>
2018-12-07 20:27:35 +0100

    Initial commit

---
+++ MarketplacePreview/Window.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace MarketplacePreview
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadPreview = new System.Windows.Forms.Button();
+            this.pictureResult = new System.Windows.Forms.PictureBox();
+            this.author = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.price = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rating = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.thumbnailPath = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.selectThumbnail = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title:";
+            // 
+            // title
+            // 
+            this.title.Location = new System.Drawing.Point(12, 41);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(302, 20);
+            this.title.TabIndex = 1;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Enabled = false;
+            this.saveButton.Location = new System.Drawing.Point(179, 242);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(138, 23);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.Save);
+            // 
+            // loadPreview
+            // 
+            this.loadPreview.Location = new System.Drawing.Point(12, 242);
+            this.loadPreview.Name = "loadPreview";
+            this.loadPreview.Size = new System.Drawing.Size(161, 23);
+            this.loadPreview.TabIndex = 4;
+            this.loadPreview.Text = "Load Preview";
+            this.loadPreview.UseVisualStyleBackColor = true;
+            this.loadPreview.Click += new System.EventHandler(this.LoadPreview);
+            // 
+            // pictureResult
+            // 
+            this.pictureResult.Image = global::MarketplacePreview.Properties.Resources.MainPanel;
+            this.pictureResult.Location = new System.Drawing.Point(323, 25);
+            this.pictureResult.Name = "pictureResult";
+            this.pictureResult.Size = new System.Drawing.Size(305, 242);
+            this.pictureResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureResult.TabIndex = 3;
+            this.pictureResult.TabStop = false;
+            // 
+            // author
+            // 
+            this.author.Location = new System.Drawing.Point(12, 83);
+            this.author.Name = "author";
+            this.author.Size = new System.Drawing.Size(302, 20);
+            this.author.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Author:";
+            // 
+            // price
+            // 
+            this.price.Location = new System.Drawing.Point(12, 121);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(302, 20);
+            this.price.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Price:";
+            // 
+            // rating
+            // 
+            this.rating.Location = new System.Drawing.Point(12, 162);
+            this.rating.Name = "rating";
+            this.rating.Size = new System.Drawing.Size(302, 20);
+            this.rating.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Rating:";
+            // 
+            // thumbnailPath
+            // 
+            this.thumbnailPath.Location = new System.Drawing.Point(12, 200);
+            this.thumbnailPath.Name = "thumbnailPath";
+            this.thumbnailPath.Size = new System.Drawing.Size(266, 20);
+            this.thumbnailPath.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Thumbnail:";
+            // 
+            // selectThumbnail
+            // 
+            this.selectThumbnail.Location = new System.Drawing.Point(281, 198);
+            this.selectThumbnail.Name = "selectThumbnail";
+            this.selectThumbnail.Size = new System.Drawing.Size(33, 23);
+            this.selectThumbnail.TabIndex = 13;
+            this.selectThumbnail.Text = "...";
+            this.selectThumbnail.UseVisualStyleBackColor = true;
+            this.selectThumbnail.Click += new System.EventHandler(this.SelectThumbnail);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 277);
+            this.Controls.Add(this.selectThumbnail);
+            this.Controls.Add(this.thumbnailPath);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.rating);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.price);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.author);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.loadPreview);
+            this.Controls.Add(this.pictureResult);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.label1);
+            this.Name = "Window";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.PictureBox pictureResult;
+        private System.Windows.Forms.Button loadPreview;
+        private System.Windows.Forms.TextBox author;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox rating;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox thumbnailPath;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button selectThumbnail;
+    }
+}
+
---
+++ MarketplacePreview/Window.cs
@@ -0,0 +1,67 @@
+﻿using MarketplacePreview.Properties;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace MarketplacePreview
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadPreview(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(Resources.MainPanel);
+            Graphics graphics = Graphics.FromImage(image);
+            StringFormat far = new StringFormat
+            {
+                Alignment = StringAlignment.Far
+            };
+            StringFormat near = new StringFormat
+            {
+                Alignment = StringAlignment.Near
+            };
+            try
+            {
+                Bitmap thumbnail = new Bitmap(Image.FromFile(thumbnailPath.Text), new Size(301, 170));
+                graphics.DrawImage(thumbnail, new Point(2, 2));
+                graphics.DrawString(price.Text, new Font("Segoe WP", 12), Brushes.Yellow, 275, 217, far);
+                graphics.DrawString(author.Text, new Font("Segoe WP", 12), Brushes.DarkGray, 25, 195, near);
+                graphics.DrawString(rating.Text, new Font("Segoe WP Semibold", 11), Brushes.White, 30, 217, far);
+                graphics.DrawString(title.Text, new Font("Segoe WP", 12), Brushes.White, 3, 175, near);
+                pictureResult.Image = image;
+                saveButton.Enabled = true;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Save(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Image File | *.png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+                pictureResult.Image.Save(dialog.FileName, ImageFormat.Png);
+        }
+
+        private void SelectThumbnail(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                Title = "Select Thumbnail",
+                Filter = "Image Files (*.png) | *.png"
+            };
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                thumbnailPath.Text = fileDialog.FileName;
+            }
+        }
+    }
+}
